308ee703f14af6f315d9ddb583a83a3a5ce32cdb
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-01-20 13:07:55 +0100

    clean up

--- yaml/ftwyaml.go
+++ yaml/ftwyaml.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 
-	//"github.com/davecgh/go-spew/spew"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -24,13 +23,9 @@ func ParseFile(filename string) (Yaml, error) {
 		return yamlConfig, err
 	}
 
-	//yamlConfig.Tests[0].File = filename
-
 	for i := range yamlConfig.Tests {
 		yamlConfig.Tests[i].File = filename
 	}
-
-	//spew.Dump(yamlConfig)
 
 	return yamlConfig, nil
 }
@@ -40,7 +35,6 @@ func ParseFiles(dirname string) []Yaml {
 	var yamls []Yaml
 
 	walkFunc := func(path string, fi os.FileInfo, err error) error {
-		//filename := path + "/" + fi.Name()
 		if !fi.IsDir() {
 			yaml, err := ParseFile(path)
 			if err != nil {
@@ -53,12 +47,6 @@ func ParseFiles(dirname string) []Yaml {
 	}
 
 	filepath.Walk(dirname, walkFunc)
-
-	//fileInfos, err := dir.Readdir(-1) // -1 means return all entries
-	//if err != nil {
-	//	fmt.Fprintf(os.Stderr, "%s\n", err)
-	//	os.Exit(1)
-	//}
 
 	return yamls
 }
