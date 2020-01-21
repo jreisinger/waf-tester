cb1a6e09a9e08e4fbdeb0a47b6103cd80aa71e28
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-01-21 16:14:49 +0100

    we can have both int and int array in a field

--- yaml/types.go
+++ yaml/types.go
@@ -1,5 +1,25 @@
 package yaml
 
+// Some FTW YAML fields (like output.status) can be both an int or array of ints. Adapted from https://github.com/go-yaml/yaml/issues/100#issuecomment-324964723.
+
+type intArray []int
+
+func (a *intArray) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var multi []int
+	err := unmarshal(&multi)
+	if err != nil {
+		var single int
+		err := unmarshal(&single)
+		if err != nil {
+			return err
+		}
+		*a = []int{single}
+	} else {
+		*a = multi
+	}
+	return nil
+}
+
 // Yaml represents a WAF test written in YAML format.
 type Yaml struct {
 	Tests []struct {
@@ -15,7 +35,8 @@ type Yaml struct {
 					Data    string
 				}
 				Output struct {
-					Status []int `yaml:"-"`
+					// can be both int or array of ints
+					Status intArray
 				}
 			}
 		}
