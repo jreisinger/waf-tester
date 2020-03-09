b21e72f824032e7dda14dc89f8a7397da7c0652c
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-03-09 21:37:35 +0100

    test GetTests

---
+++ httptest/httptest_test.go
@@ -0,0 +1,13 @@
+package httptest
+
+import "testing"
+
+func TestGetTests(t *testing.T) {
+	tests, err := GetTests("../tests", "", "https")
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if len(tests) == 0 {
+		t.Errorf("tests: want some tests, got %d tests", len(tests))
+	}
+}
