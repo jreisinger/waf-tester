8db1410ccc94c620a672ddb740df49ececbad22d
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-03-09 21:17:27 +0100

    minor changes in main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	// Get tests to execute.
 	alltests, err := httptest.GetTests(flags.TestsPath, flags.Only, flags.Scheme)
 	if err != nil {
-		log.Fatalf("can't get tests: %v", err)
+		log.Fatalf("cannot get tests: %v", err)
 	}
 	log.Printf("flags: %+v", alltests)
 	/*
