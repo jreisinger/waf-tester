c487a79be855c30dc77fc9918ce7f07c69a9b2b6
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-03-11 18:11:42 +0100

    minor changes in yaml/ftwyaml.go

--- yaml/ftwyaml.go
+++ yaml/ftwyaml.go
@@ -10,6 +10,27 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
+// ParseFiles parses all YAML files in a directory.
+func ParseFiles(path string) []Yaml {
+	var yamls []Yaml
+
+	walkFunc := func(path string, fi os.FileInfo, err error) error {
+		if !fi.IsDir() && isYaml(path) {
+			yaml, err := ParseFile(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "warn parsing %s: %s\n", path, err)
+				return err
+			}
+			yamls = append(yamls, yaml)
+		}
+		return nil
+	}
+
+	filepath.Walk(path, walkFunc)
+
+	return yamls
+}
+
 // ParseFile parses a single YAML file.
 func ParseFile(filename string) (Yaml, error) {
 	var yamlConfig Yaml
@@ -36,25 +57,4 @@ func isYaml(path string) bool {
 		filepath.Ext(path) == ".yml" ||
 		filepath.Ext(path) == ".YAML" ||
 		filepath.Ext(path) == ".YML"
-}
-
-// ParseFiles parses all YAML files in a directory.
-func ParseFiles(path string) []Yaml {
-	var yamls []Yaml
-
-	walkFunc := func(path string, fi os.FileInfo, err error) error {
-		if !fi.IsDir() && isYaml(path) {
-			yaml, err := ParseFile(path)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "warn parsing %s: %s\n", path, err)
-				return err
-			}
-			yamls = append(yamls, yaml)
-		}
-		return nil
-	}
-
-	filepath.Walk(path, walkFunc)
-
-	return yamls
 }
