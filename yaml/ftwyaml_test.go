84c6bdcfe1a04254a2f90f81fb526bfdfe79e65d
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-03-11 18:24:18 +0100

    add TestIsYaml

---
+++ yaml/ftwyaml_test.go
@@ -0,0 +1,34 @@
+package yaml
+
+import (
+	"testing"
+)
+
+func TestIsYaml(t *testing.T) {
+	type testpair struct {
+		filePath string
+		result   bool
+	}
+
+	testpairs := []testpair{
+		{"file.yaml", true},
+		{"file.yml", true},
+		{"file.YAML", true},
+		{"file.yaml", true},
+		{"file.txt", false},
+		{"path/to/file.yaml", true},
+		{"", false},
+		{"yaml", false},
+	}
+
+	for _, pair := range testpairs {
+		result := isYaml(pair.filePath)
+		if result != pair.result {
+			t.Error(
+				"For", pair.filePath,
+				"expected", pair.result,
+				"got", result,
+			)
+		}
+	}
+}
