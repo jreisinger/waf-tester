1336ba8436e65b794c511969badc5b71906b3807
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-03-11 17:47:57 +0100

    add TestIntInSlice

--- httptest/util_test.go
+++ httptest/util_test.go
@@ -8,3 +8,17 @@ func TestGenUUID(t *testing.T) {
 		t.Errorf("genUUID: want 16-byte string, got %d-byte string", len(uuid))
 	}
 }
+
+func TestIntInSlice(t *testing.T) {
+	s := []int{0, -3, 100, 42, 27, 3}
+
+	n := 42
+	if !intInSlice(n, s) {
+		t.Error("intInSlice: want true, got false")
+	}
+
+	n = 1000
+	if intInSlice(n, s) {
+		t.Error("intInSlice: want false, got true")
+	}
+}
