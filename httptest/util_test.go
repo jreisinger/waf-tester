9ffb3b36b86ce980b3acdee5cc8de1bf42a8586f
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-03-11 18:06:08 +0100

    add TestStringInSlice

--- httptest/util_test.go
+++ httptest/util_test.go
@@ -10,15 +10,29 @@ func TestGenUUID(t *testing.T) {
 }
 
 func TestIntInSlice(t *testing.T) {
-	s := []int{0, -3, 100, 42, 27, 3}
+	is := []int{0, -3, 100, 42, 27, 3}
 
-	n := 42
-	if !intInSlice(n, s) {
+	i := 42
+	if !intInSlice(i, is) {
 		t.Error("intInSlice: want true, got false")
 	}
 
-	n = 1000
-	if intInSlice(n, s) {
+	i = 1000
+	if intInSlice(i, is) {
 		t.Error("intInSlice: want false, got true")
+	}
+}
+
+func TestStringInSlice(t *testing.T) {
+	ss := []string{"repetitio", "est", "mater", "studorium"}
+
+	s := "est"
+	if !stringInSlice(s, ss) {
+		t.Error("stringInSlice: want true, got false")
+	}
+
+	s = "veritas"
+	if stringInSlice(s, ss) {
+		t.Error("stringInSlice: want false, got true")
 	}
 }
